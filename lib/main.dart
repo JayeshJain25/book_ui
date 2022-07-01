57298968bc3c3bd0cec797ad4f40bbbd23329637
jayeshjain25 <jayeshkjain6@gmail.com>
2022-07-01 12:38:18 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:book_ui/consttants.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/home_screen.dart';
+import 'widgets/rounded_button.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Book App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: Theme.of(context).textTheme.apply(
+              displayColor: kBlackColor,
+            ),
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/Bitmap.png"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.headline2,
+                children: const [
+                  TextSpan(
+                    text: "flamin",
+                  ),
+                  TextSpan(
+                    text: "go.",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .6,
+              child: RoundedButton(
+                text: "start reading",
+                fontSize: 20,
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return HomeScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,301 @@
+import 'package:book_ui/consttants.dart';
+import 'package:book_ui/screens/details_screen.dart';
+import 'package:book_ui/widgets/book_rating.dart';
+import 'package:book_ui/widgets/reading_card_list.dart';
+import 'package:book_ui/widgets/two_side_rounded_button.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/main_page_bg.png"),
+                  alignment: Alignment.topCenter,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: size.height * .1),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: RichText(
+                      text: TextSpan(
+                        style: Theme.of(context).textTheme.headline4,
+                        children: const [
+                          TextSpan(text: "What are you \nreading "),
+                          TextSpan(
+                              text: "today?",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ))
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: <Widget>[
+                        ReadingListCard(
+                          pressRead: () {},
+                          image: "assets/images/book-1.png",
+                          title: "Crushing & Influence",
+                          auth: "Gary Venchuk",
+                          rating: 4.9,
+                          pressDetails: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return const DetailsScreen();
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                        ReadingListCard(
+                          image: "assets/images/book-2.png",
+                          title: "Top Ten Business Hacks",
+                          auth: "Herman Joel",
+                          rating: 4.8,
+                          pressRead: () {},
+                          pressDetails: () {},
+                        ),
+                        const SizedBox(width: 30),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        RichText(
+                          text: TextSpan(
+                            style: Theme.of(context).textTheme.headline4,
+                            children: const [
+                              TextSpan(text: "Best of the "),
+                              TextSpan(
+                                text: "day",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                        bestOfTheDayCard(size, context),
+                        RichText(
+                          text: TextSpan(
+                            style: Theme.of(context).textTheme.headline4,
+                            children: [
+                              const TextSpan(text: "Continue "),
+                              const TextSpan(
+                                text: "reading...",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Container(
+                          height: 80,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(38.5),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: const Offset(0, 10),
+                                blurRadius: 33,
+                                color: const Color(0xFFD3D3D3).withOpacity(.84),
+                              ),
+                            ],
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(38.5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 30, right: 20),
+                                    child: Row(
+                                      children: <Widget>[
+                                        Expanded(
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              const Text(
+                                                "Crushing & Influence",
+                                                style: const TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              const Text(
+                                                "Gary Venchuk",
+                                                style: const TextStyle(
+                                                  color: kLightBlackColor,
+                                                ),
+                                              ),
+                                              const Align(
+                                                alignment:
+                                                    Alignment.bottomRight,
+                                                child: const Text(
+                                                  "Chapter 7 of 10",
+                                                  style: TextStyle(
+                                                    fontSize: 10,
+                                                    color: kLightBlackColor,
+                                                  ),
+                                                ),
+                                              ),
+                                              const SizedBox(height: 5),
+                                            ],
+                                          ),
+                                        ),
+                                        Image.asset(
+                                          "assets/images/book-1.png",
+                                          width: 55,
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  height: 7,
+                                  width: size.width * .65,
+                                  decoration: BoxDecoration(
+                                    color: kProgressIndicator,
+                                    borderRadius: BorderRadius.circular(7),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 40),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container bestOfTheDayCard(Size size, BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 20),
+      width: double.infinity,
+      height: 245,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.only(
+                left: 24,
+                top: 24,
+                right: size.width * .35,
+              ),
+              height: 230,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: const Color(0xFFEAEAEA).withOpacity(.45),
+                borderRadius: BorderRadius.circular(29),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: const Text(
+                      "New York Time Best For 11th March 2020",
+                      style: TextStyle(
+                        fontSize: 9,
+                        color: kLightBlackColor,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "How To Win \nFriends &  Influence",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  const Text(
+                    "Gary Venchuk",
+                    style: TextStyle(color: kLightBlackColor),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 10.0),
+                    child: Row(
+                      children: <Widget>[
+                        const Padding(
+                          padding: const EdgeInsets.only(right: 10.0),
+                          child: const BookRating(score: 4.9),
+                        ),
+                        const Expanded(
+                          child: const Text(
+                            "When the earth was flat and everyone wanted to win the game of the best and people….",
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 10,
+                              color: kLightBlackColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            right: 0,
+            top: 0,
+            child: Image.asset(
+              "assets/images/book-3.png",
+              width: size.width * .37,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: SizedBox(
+              height: 40,
+              width: size.width * .3,
+              child: TwoSideRoundedButton(
+                text: "Read",
+                radious: 24,
+                press: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
